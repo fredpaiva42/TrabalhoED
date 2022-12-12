de54ede09f79f256078cb21d34d91173c989cad8
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-12-12 19:31:57 -0300

    Mudei os campos

--- Exemplo.c
+++ Exemplo.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Treinador {
-    int primaryKey;
+    int registration;
     char name[50];
     int age;
 } Ttrainer;
 
 typedef struct Pokemon {
-    int primaryKey;
+    int pokeID;
     char name[50];
     char type[50];
 } Tpoke;
 
 typedef struct Relationship {
-    int tPrimaryKey;
-    int pokePrimaryKey;
+    int registrationPK;
+    int pokeIDPK;
     int level;
     char gender[50];
 } Trel;
 
 //Recebe um objeto do tipo trainer e um arquivo para escrever, e escreve o objeto no arquivo.
 void saveTrainer(Ttrainer *trainer, FILE *out) {
-    fwrite(&trainer->primaryKey, sizeof(int), 1, out);
+    fwrite(&trainer->registration, sizeof(int), 1, out);
     fwrite(trainer->name, sizeof(char), sizeof(trainer->name), out);
     fwrite(&trainer->age, sizeof(int), 1, out);
 }
 
 //Recebe um objeto do tipo pokémon e um arquivo para escrever, e escreve o objeto no arquivo.
 void savePoke(Tpoke *poke, FILE *out) {
-    fwrite(&poke->primaryKey, sizeof(int), 1, out);
+    fwrite(&poke->pokeID, sizeof(int), 1, out);
     fwrite(poke->name, sizeof(char), sizeof(poke->name), out);
     fwrite(poke->type, sizeof(char), sizeof(poke->type), out);
 }
@@ -37,7 +38,7 @@ void savePoke(Tpoke *poke, FILE *out) {
 Ttrainer *readTrainer(FILE *in) {
     Ttrainer *trainer = (Ttrainer *) malloc(sizeof(Ttrainer)); 
     //Caso a primary key não exista no arquivo returna NULL.
-    if (fread(&trainer->primaryKey, sizeof(int), 1, in) <= 0) {
+    if (fread(&trainer->registration, sizeof(int), 1, in) <= 0) {
         free(trainer);
         return NULL;
     }
@@ -52,7 +53,7 @@ Ttrainer *readTrainer(FILE *in) {
 Tpoke *readPoke(FILE *in) {
     Tpoke *poke = (Tpoke *) malloc(sizeof(Tpoke)); 
     //Caso a primary key não exista no arquivo returna NULL.
-    if (fread(&poke->primaryKey, sizeof(int), 1, in) <= 0) {
+    if (fread(&poke->pokeID, sizeof(int), 1, in) <= 0) {
         free(poke);
         return NULL;
     }
